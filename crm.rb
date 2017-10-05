a237a81527f64750d30e5d89a2e815d8861286d8
Meera <mjvsharma@outlook.com>
2017-10-05 14:38:52 -0400

    made basic crm page linking to about, contacts

--- crm.rb
+++ crm.rb
@@ -7,3 +7,32 @@ get '/' do
 erb :index
 
 end
+
+get '/contacts' do
+  @all_contacts = Contact.all
+  #Model.all(options = nil) finds all the records matching the supplied options.
+  #If no options are supplied, all rows from the database are returned.
+  #all database rows are lso returned as an array
+
+
+  erb :contacts
+
+
+
+end
+
+get '/about' do
+
+  erb :about
+
+
+end  
+
+
+
+
+
+
+after do
+  ActiveRecord::Base.connection.close
+end
