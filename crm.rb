76461bde6d1442f2a2c445f2ceccf37134098669
Meera <mjvsharma@outlook.com>
2017-10-07 11:22:22 -0400

    made separate page for each contact, linked each contact's own page to the contacts.erb

--- crm.rb
+++ crm.rb
@@ -21,12 +21,31 @@ get '/contacts' do
 
 end
 
+
+get '/contacts/:id' do
+  #params[:id] params is a hash. id is the key and we retrieve the value from it
+
+  @contact = Contact.find_by({id: params[:id].to_i})
+  #this is gonna fetch the contact witht he matching id
+
+  if @contact
+    erb :show_contact
+  else
+    raise Sinatra::NotFound
+  end
+
+  
+
+end
+
+
+
 get '/about' do
 
   erb :about
 
 
-end  
+end
 
 
 
@@ -36,3 +55,14 @@ end
 after do
   ActiveRecord::Base.connection.close
 end
+
+
+#crm part 3
+# we want the userts to see indiivudal contact dtails on their own page
+# to do this we establish a dynamic route, whch has the user id in it
+#
+#
+# to tell sinatra what would happen if we enter diffeent id urls
+# u need to do :id after slash
+#
+# therefore, creae a new view called show_contacts.erb so that this is where its gonna redirect
